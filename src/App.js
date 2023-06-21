4686373198730539d805d747bade1f33f6c9666a
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-06-21 12:43:38 +0200

    add checkHandle function to check box.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import products from "./components/Products";
 import {useState} from "react";
 
 const App = function () {
-    // state
-    const [isLoggedIn , setState] = useState(true);
-    let logInStatus;
-    const handleClick = () => {
-        setState(!isLoggedIn ? true : false);
-        if (logInStatus = isLoggedIn ? "in" : "out"){
-        console.log(logInStatus) ;
-        }
-    };
-    const todoItems = products.map(product => <TodoItem key={product.id} item={product} />)
+
+    const [state , setState] = useState(products);
+    const todoItems = products.map(product => <TodoItem  key={product.id} item={product} />)
     return(
         <div className="todo-list">
             {todoItems}
-            <button onClick={handleClick}>log in</button>
         </div>
     )
 };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {useState} from "react";
 
 const TodoItem = function ({item}) {
 
+    const [change , setChange] = useState(false);
+    const checkHandle = () =>{
+        setChange(change ? false : true);
+    };
+
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={item.completed}/>
+            <input type="checkbox" checked={change} onChange={checkHandle}/>
             <p>{item.id}-{item.name} : {item.price}</p>
         </div>
     )
